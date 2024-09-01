9079d14ae455e7e2fd4cba90a75438b1386c1ab2
Aruneshwar005 <159513842+Aruneshwar005@users.noreply.github.com>
2024-09-01 20:20:46 +0530

    dbConfig.js

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.mongo_url)
+
+mongoose.set('strictQuery', false);
+
+// Corrected the environment variable name to match the .env file
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
 
 const connection = mongoose.connection;
 
-connection.on('connected' , ()=>{
-    console.log('Mongo DB Connetion Successfull');
-})
+connection.on('connected', () => {
+    console.log('MongoDB Connection Successful');
+});
 
-connection.on('error' , (err)=>{
-    console.log('Mongo DB Connetion Failed');
-})
+connection.on('error', (err) => {
+    console.log('MongoDB Connection Failed:', err);
+});
